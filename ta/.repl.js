db163d92249abc398698a3c2fda9dc29092207ab
John BEPPU <john.beppu@gmail.com>
2019-09-25 15:43:04 -0700

    Add global.alerts and global.time to repl
    Also add a profitLoss function for my convenience.

--- ta/.repl.js
+++ ta/.repl.js
@@ -18,4 +18,16 @@ global.ccxt        = require('ccxt')
 global.talib       = require('talib')
 
 global.ta = require('./index')
+global.alerts = require('./alerts')
+global.time = require('./time')
+
+// XXX - This function works for XBTUSD, but what about other markets?
+global.profitLoss = function profitLoss(quantity, entry, exit, leverage, short) {
+  const entryValue = quantity / entry
+  const exitValue  = (exit / entry) * entryValue
+  const profitLoss = short ? entryValue - exitValue : exitValue - entryValue
+  const profitLossPercent = short ? (exitValue / entryValue * 100) : (entryValue / exitValue * 100)
+  const roe = profitLossPercent * leverage
+  return { entryValue, exitValue, profitLoss, profitLossPercent, roe }
+}
 
