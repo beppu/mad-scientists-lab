101d9974efe2bcbdaef0a4249ae543891d3b014d
John BEPPU <john.beppu@gmail.com>
2019-09-25 10:55:10 -0700

    Add time calculation functions

---
+++ ta/time.js
@@ -0,0 +1,31 @@
+
+function timeframeToMinutes(timeframe) {
+  const match  = timeframe.match(/(\d+)(\w+)/);
+  if (!match) {
+    throw new Error(`Invalid timeframe: '${timeframe}'`);
+  }
+  const nu = match[1];
+  const unit = match[2];
+  const n = Math.min(parseInt(nu, 10));
+  switch (unit) {
+  case 'm':
+    return n;
+  case 'h':
+    return 60 * n;
+  case 'd':
+    return 24 * 60 * n;
+  }
+  throw new Error(`Unsupported timeframe: '${timeframe}'`);
+}
+
+
+function timestampForTimeframe(timeframe, ms) {
+  const ints = timeframeToMinutes(timeframe) * 60 * 1000;
+  const diff = ms % ints;
+  return ms - diff;
+}
+
+module.exports = {
+  timeframeToMinutes,
+  timestampForTimeframe
+}
