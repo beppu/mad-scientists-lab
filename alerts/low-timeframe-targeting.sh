c637356999011b9906a98e17fc5f0ac14c4367f9
John BEPPU <john.beppu@gmail.com>
2019-09-27 17:45:13 -0700

    Keep an eye on 3m 200 SMA relative to price

--- alerts/low-timeframe-targeting.sh
+++ alerts/low-timeframe-targeting.sh
@@ -39,3 +39,9 @@ aligned --timeframe 3m sma 50 100 200 \
   && alert --timeframe 3m --webhook $ALERT_BULLISH_ALIGNED "50/100/200 SMA in bullish alignment"
 aligned --timeframe 3m sma 200 100 50 \
   && alert --timeframe 3m --webhook $ALERT_BEARISH_ALIGNED "50/100/200 SMA in bearish alignment"
+
+# sma 200 on 3m
+price --timeframe 3m --gt sma 200 \
+  && alert --timeframe 3m --webhook $WEBHOOK "Price above 200 SMA"
+price --timeframe 3m --lt sma 200 \
+  && alert --timeframe 3m --webhook $WEBHOOK "Price below 200 SMA"
