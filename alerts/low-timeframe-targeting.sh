dedfcbd6adda1680b451d6e202171673baa22187
John BEPPU <john.beppu@gmail.com>
2019-10-02 13:36:27 -0700

    Add timeframe to alert messages

--- alerts/low-timeframe-targeting.sh
+++ alerts/low-timeframe-targeting.sh
@@ -11,13 +11,13 @@ echo "> analyzing $TA_EXCHANGE + $TA_MARKET on $TA_TIMEFRAME timeframe"
 
 # 50/100/200 alignment
 aligned sma 50 100 200 \
-  && alert --webhook $ALERT_BULLISH_ALIGNED "50/100/200 SMA in bullish alignment"
+  && alert --webhook $ALERT_BULLISH_ALIGNED "$TA_TIMEFRAME 50/100/200 SMA in bullish alignment"
 aligned sma 200 100 50 \
-  && alert --webhook $ALERT_BEARISH_ALIGNED "50/100/200 SMA in bearish alignment"
+  && alert --webhook $ALERT_BEARISH_ALIGNED "$TA_TIMEFRAME 50/100/200 SMA in bearish alignment"
 
 # price vs sma 200
 price --gt sma 200 \
-  && alert --webhook $ALERT_SMALL "Price above 200 SMA"
+  && alert --webhook $ALERT_SMALL "Price above $TA_TIMEFRAME 200 SMA"
 price --lt sma 200 \
-  && alert --webhook $ALERT_SMALL "Price below 200 SMA"
+  && alert --webhook $ALERT_SMALL "Price below $TA_TIMEFRAME 200 SMA"
 
