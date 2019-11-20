dffb6a46f65a0b3a668669bfddfc6de0853e9047
John BEPPU <john.beppu@gmail.com>
2019-11-19 18:39:02 -0800

    Be able to detect regular bullish divergence
    It's a little rough, but I finally implemented bband filters for detecting
    extremes accurately.  It'll never be perfect due to support levels that override
    bbands that the script would have a hard time knowing about, but it should work
    most of the time.
    
    It feels good.

--- ta/utils.js
+++ ta/utils.js
@@ -30,7 +30,71 @@ function isDescending(comparables) {
   return result.isDescending
 }
 
+/**
+ * Trying to find lows by comparing lows to their proximity to the lower bband
+ * @param {Number} threshold - Distance from lowerBand.  Positive values allow prices to be higher than the lower band.
+ * @returns {Function} A function that takes (imd, i) and returns a boolean
+ */
+function lowEnoughFn(threshold) {
+  return function (imd, i) {
+    return imd.low[i] - imd.lowerBand[i] <= threshold
+  }
+}
+
+/**
+ * Return an array or array of indices of consecutive candles that satisfy the given function
+ * @param {InvertedMarketData} imd - inverted market data
+ * @param {Function} fn - a function that takes imd and an integer index and returns a boolean
+ * @returns {Array<Array<Number>>} An array of array of indices that satisfy fn
+ */
+function findClusters(imd, fn) {
+  let result = []
+  let cluster = []
+  for (let i = 0; i < imd.close.length; i++) {
+    if (fn(imd, i)) {
+      cluster.push(i)
+    } else {
+      if (cluster.length) {
+        result.push(cluster)
+        cluster = []
+      }
+    }
+  }
+  if (cluster.length) {
+    result.push(cluster)
+  }
+  return result
+}
+
+/**
+ * Out of the cluster, which index had the lowest close
+ * @param {InvertedMarketData} imd - inverted market data
+ * @param {Array<Number>} cluster - an array of indices
+ */
+function findLocalLow(imd, cluster) {
+  const closes = cluster.map((i) => imd.close[i])
+  const lowest = Math.min(...closes)
+  const lowestIndex = closes.findIndex((c) => c == lowest)
+  return cluster[lowestIndex]
+}
+
+/**
+ * Out of the cluster, which index had the highest close
+ * @param {InvertedMarketData} imd - inverted market data
+ * @param {Array<Number>} cluster - an array of indices
+ */
+function findLocalHigh(imd, cluster) {
+  const closes = cluster.map((i) => imd.close[i])
+  const highest = Math.max(...closes)
+  const highestIndex = closes.findIndex((c) => c == highest)
+  return cluster[highestIndex]
+}
+
 module.exports = {
   isAscending,
-  isDescending
+  isDescending,
+  lowEnoughFn,
+  findClusters,
+  findLocalLow,
+  findLocalHigh
 }
