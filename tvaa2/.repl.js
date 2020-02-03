2d97e74142aedf12b50905ee2a640f17964019ae
John BEPPU <john.beppu@gmail.com>
2020-02-02 21:44:28 -0800

    Add bin/repl for debugging convenience

---
+++ tvaa2/.repl.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+function reload(module){
+  delete require.cache[require.resolve(module)]
+  return require(module)
+}
+
+global.reload   = reload
+global.rl       = reload
+global.cl       = console.log
+
+// global.Promise     = require('bluebird')
+// global.Lazy        = require('lazy.js')
+// global.moment      = require('moment')
+// global.sprintf     = require('sprintf')
+// global.outdent     = require('outdent')
+
+global.DB = require('./lib/db')
+global.alert = require('./lib/alert')
+global.audio = require('./lib/audio')
