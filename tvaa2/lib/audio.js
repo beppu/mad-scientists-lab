0c405852171523fce445eefd316dd0f46df9fb42
John BEPPU <john.beppu@gmail.com>
2020-02-11 19:44:36 -0800

    Apply volume settings to espeak
    Also, apply an exponential function to make espeak volume
    match mplayer volume more closely.

--- tvaa2/lib/audio.js
+++ tvaa2/lib/audio.js
@@ -9,8 +9,17 @@ async function play(file, opts) {
   await exec(`mplayer -volume ${volume} ${file}`)
 }
 
+// tvaa2 volume is 0-100 as understood by mplayer
+// espeak volume is 0-200 but it doesn't scale the same way mplayer does.
+// applying this exponential function was my attempt to make espeak volumes match mplayer's volume
+function _espeakVolume(v) {
+  return 0.02 * (v ** 2)
+}
+
 async function say(message, opts) {
-  const espeak = spawn('espeak', [], { stdio: ['pipe', process.stdout, process.stderr]})
+  const db = await DB.instance()
+  const volume = _espeakVolume(db.get('config.volume').value())
+  const espeak = spawn('espeak', ['-a', volume], { stdio: ['pipe', process.stdout, process.stderr]})
   await streamWrite(espeak.stdin, message + "\n")
   await streamEnd(espeak.stdin)
   await onExit(espeak)
