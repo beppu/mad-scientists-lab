6741b2fc9e273a861af474f1901e00e3584024e5
John BEPPU <john.beppu@gmail.com>
2020-03-05 17:39:41 -0800

    Test to see if pipeline.mainLoopFn is doing its math correctly
    It's not yet completely right.

--- ta/pipeline.unit.test.js
+++ ta/pipeline.unit.test.js
@@ -1,4 +1,5 @@
 const {DateTime} = require('luxon')
+const talib = require('talib')
 const pipeline = require('./pipeline')
 const ta = require('./index')
 
@@ -74,6 +75,32 @@ test("Aggregation should combine candles correctly", () => {
 })
 
 test("pipeline.mainLoopFn should be able to take an empty indicatorSpecs", () => {
-  let state
   const s = pipeline.mainLoopFn('1h', {  })
+  // If it doesn't crash, we're good.
+})
+
+test("pipeline.mainLoopFn should return a function that calculates correctly", () => {
+  let state
+  const baseTimeframe = '1h'
+  const indicatorSpecs = { '1h': [ ['rsi', 14], ['sma', 20] ], '2h': [ ['bbands', 20 ], ['ema', 9] ] }
+  const iterate = pipeline.mainLoopFn(baseTimeframe, indicatorSpecs)
+  candles.forEach((c) => state = iterate(c))
+  expect(typeof iterate).toEqual("function")
+  expect(state.baseTimeframe).toEqual(baseTimeframe)
+  expect(state.md1h.timestamp).toHaveLength(candles.length)
+  expect(state.imd1h.high).toHaveLength(candles.length)
+  expect(state.aggregator1h).toBeUndefined()
+  expect(state.indicators1h).toHaveLength(2)
+  expect(state.indicators2h).toHaveLength(2)
+  expect(state.imd2h.close).toHaveLength(13)
+  expect(state.imd2h.ema9).toHaveLength(5)
+  expect(state.imd2h.upperBands).toBeUndefined() // There shouldn't be enough candles to calculate bbands yet.
+
+  // The real test will be comparing against talib's calculations.
+  const ma = 'ema', period = 9
+  const indicatorSettings  = ta.id[ma](state.md2h, period)
+  const r                  = talib.execute(indicatorSettings)
+  const invertedMarketData = ta.invertedMarketData(state.md2h)
+  ta.invertedAppend(invertedMarketData, 'ema9', r.result.outReal)
+  expect(invertedMarketData.ema9).toEqual(state.imd2h.ema9)
 })
