f5c362a52ee01abab267a2b5ab57502f6df29d7a
John BEPPU <john.beppu@gmail.com>
2020-03-19 17:52:55 -0700

    Add a basic unit test for BuyAndHold

---
+++ ta/strategies/BuyAndHold.unit.test.js
@@ -0,0 +1,8 @@
+const BuyAndHold = require('./BuyAndHold')
+const kindOf = require('kind-of')
+
+test("a strategy when initialized should return its indicatorSpecs and a strategy function", () => {
+  const [indicatorSpecs, strategy] = BuyAndHold('1h', { balance: 100000 })
+  expect(indicatorSpecs).toMatchObject({ "1h": [] })
+  expect(kindOf(strategy)).toBe('function')
+})
