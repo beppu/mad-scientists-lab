51a51f972c37dc22413d8682d998ece1cc00f137
John BEPPU <john.beppu@gmail.com>
2020-03-25 03:50:18 -0700

    Let's see if we can detect divergences
    It needs tuning.
    I noticed a lot of false positives.

--- ta/strategies/DivergenceConfluence.js
+++ ta/strategies/DivergenceConfluence.js
@@ -4,15 +4,32 @@
  * However, my human weaknesses can make execution difficult at times.
  * I want to encode the best me in this strategy.
  */
+
+const analysis = require('../analysis')
+const time = require('../time')
+
 module.exports = function init(baseTimeframe, config) {
+  const { logger, balance } = config
   const indicatorSpecs = {
+    /*
     '3m':  [ ['rsi'], ['bbands'] ],
     '5m':  [ ['rsi'], ['bbands'] ],
     '10m': [ ['rsi'], ['bbands'] ],
     '15m': [ ['rsi'], ['bbands'] ],
+    */
+    '1d':  [ ['rsi'], ['bbands'] ],
+  }
+  const divergenceOptions = {
+    ageThreshold: 1,
+    gapThreshold: [7, 30],
+    peakThreshold: 9,
   }
   indicatorSpecs[baseTimeframe] = []
   function strategy(marketState, executedOrders) {
+    if (analysis.divergence.regularBullish(marketState.imd1d, divergenceOptions)) {
+      const ts = time.dt(marketState.imd1d.timestamp[0])
+      logger.info(`1d bullish divergence on ${ts.toISO()} at ${marketState.imd1d.low[0]}`)
+    }
     return []
   }
   return [indicatorSpecs, strategy]
