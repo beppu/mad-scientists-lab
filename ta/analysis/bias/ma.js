7aecb878ea58032909475b67d2024a97938a8d88
John BEPPU <john.beppu@gmail.com>
2020-03-27 13:22:57 -0700

    Simplify bias detection
    I've been writing so many functions that return functions that I did it
    unnecessarily in this case. Instead of generating a custom function that looks
    up the price series and moving average series, I can let the caller just hand
    them to me. He knows what he wants.

--- ta/analysis/bias/ma.js
+++ ta/analysis/bias/ma.js
@@ -4,27 +4,20 @@ const utils = require('../../utils')
  * Initialize a bias detection function using price relative to a moving average
  * and the slope of the moving average.
  *
- * @param {String} timeframe - duration of candle
- * @param {String} maType - type of moving average.  (ema or sma)
- * @param {Number} period - length of moving average
+ * @param {InvertedMarketData} imd - invertedMarketData
+ * @param {InvertedSeries|Array} maSeries - moving average to use for bias detection
  * @returns {String|undefined} bullish, bearish, or undefined
  */
-module.exports = function init(timeframe, maType, period) {
-  const imdKey = `imd${timeframe}`
-  const maKey  = `${maType}${period}`
+module.exports = function movingAverageBias(imd, maSeries) {
+  const ma    = maSeries[0]
+  const close = imd.close[0]
+  const slope = utils.slope([1, imd.close[1]], [2, imd.close[0]]) // make up x, and use closes for y
 
-  return function movingAverageBias(marketState) {
-    const imd   = marketState[imdKey]
-    const close = imd.close[0]
-    const ma    = imd[maKey][0]
-    const slope = utils.slope([1, imd.close[1]], [2, imd.close[0]]) // make up x, and use closes for y
-
-    if (close > ma && slope > 0) {
-      return 'bullish'
-    }
-    if (close < ma && slope < 0) {
-      return 'bearish'
-    }
-    return undefined
+  if (close > ma && slope > 0) {
+    return 'bullish'
   }
+  if (close < ma && slope < 0) {
+    return 'bearish'
+  }
+  return undefined
 }
