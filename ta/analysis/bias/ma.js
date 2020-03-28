88fb6a15d5222ff20501a9cb997a31e7042def39
John BEPPU <john.beppu@gmail.com>
2020-03-27 19:53:52 -0700

    Generalize bias.ma even further
    Instead of hard-coding imd.close, pass in any price series to compare against
    the moving average series.

--- ta/analysis/bias/ma.js
+++ ta/analysis/bias/ma.js
@@ -4,14 +4,14 @@ const utils = require('../../utils')
  * Initialize a bias detection function using price relative to a moving average
  * and the slope of the moving average.
  *
- * @param {InvertedMarketData} imd - invertedMarketData
+ * @param {InvertedSeries|Array} prices - prices to compare against `maSeries`
  * @param {InvertedSeries|Array} maSeries - moving average to use for bias detection
  * @returns {String|undefined} bullish, bearish, or undefined
  */
-module.exports = function movingAverageBias(imd, maSeries) {
+module.exports = function movingAverageBias(prices, maSeries) {
   const ma    = maSeries[0]
-  const close = imd.close[0]
-  const slope = utils.slope([1, imd.close[1]], [2, imd.close[0]]) // make up x, and use closes for y
+  const close = prices[0]
+  const slope = utils.slope([1, prices[1]], [2, prices[0]]) // make up x, and use closes for y
 
   if (close > ma && slope > 0) {
     return 'bullish'
