4c1dfa56c7b2d9f73d9c9f35b3ccb02aa4db1f74
John BEPPU <john.beppu@gmail.com>
2020-03-27 22:45:29 -0700

    Add a template for strategy unit tests too

---
+++ ta/strategies/Strategy.unit.test.template.js
@@ -0,0 +1,8 @@
+const Scalper = require('./Scalper')
+const kindOf = require('kind-of')
+
+test("a strategy when initialized should return its indicatorSpecs and a strategy function", () => {
+  const [indicatorSpecs, strategy] = Scalper('1h', { balance: 100000 })
+  expect(indicatorSpecs).toMatchObject({ })
+  expect(kindOf(strategy)).toBe('function')
+})
