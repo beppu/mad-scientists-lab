462946c16b6320de6880372c343bc88fcc276d61
John BEPPU <john.beppu@gmail.com>
2020-04-07 23:28:21 -0700

    Rename vars to make test work again

--- ta/strategies/DC.unit.test.js
+++ ta/strategies/DC.unit.test.js
@@ -1,8 +1,8 @@
-const DivergenceConfluence = require('./DivergenceConfluence')
+const DC = require('./DC')
 const kindOf = require('kind-of')
 
 test("a strategy when initialized should return its indicatorSpecs and a strategy function", () => {
-  const [indicatorSpecs, strategy] = DivergenceConfluence.init('1h', { balance: 100000 })
+  const [indicatorSpecs, strategy] = DC.init('1h', { balance: 100000 })
   expect(indicatorSpecs).toMatchObject({ "1h": [] })
   expect(kindOf(strategy)).toBe('function')
 })
