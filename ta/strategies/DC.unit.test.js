ea7782baea57fe903a739d30803b51362bf20515
John BEPPU <john.beppu@gmail.com>
2020-04-08 12:47:35 -0700

    Fix test to reflect new indicatorSpecs

--- ta/strategies/DC.unit.test.js
+++ ta/strategies/DC.unit.test.js
@@ -3,6 +3,6 @@ const kindOf = require('kind-of')
 
 test("a strategy when initialized should return its indicatorSpecs and a strategy function", () => {
   const [indicatorSpecs, strategy] = DC.init('1h', { balance: 100000 })
-  expect(indicatorSpecs).toMatchObject({ "1h": [] })
+  expect(indicatorSpecs).toMatchObject({ "1h": [ ['rsi'], ['bbands'] ] })
   expect(kindOf(strategy)).toBe('function')
 })
