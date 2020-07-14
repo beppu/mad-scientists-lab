eb8c9c5837ec7694cbcc9fa185b44ea805689d6a
John BEPPU <john.beppu@gmail.com>
2020-07-14 14:54:45 -0700

    Outline some tests for LimitFill to implement later

--- ta/techniques/LimitFill.unit.test.js
+++ ta/techniques/LimitFill.unit.test.js
@@ -9,3 +9,15 @@ test("Calling Limitfill.init should return a microstrategy function", () => {
   const [indicatorSpecs, microStrategy] = LimitFill.init({})
   expect(kindOf(microStrategy)).toBe("function")
 })
+
+test("LimitFill should issue a limit buy order when asked", () => {
+})
+
+test("LimitFill should move the price up if the order isn't filled within the allotted time", () => {
+})
+
+test("LimitFill should be able to handle a partial fill", () => {
+})
+
+test("LimitFill should use a market order if price moves beyond the configured threshold", () => {
+})
