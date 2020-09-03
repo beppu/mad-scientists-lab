a2dab104dad432bf086575c67141240768128c79
John BEPPU <john.beppu@gmail.com>
2020-09-03 13:15:20 -0700

    Flesh out a bybit.Driver class

--- ta/exchanges/bybit.js
+++ ta/exchanges/bybit.js
@@ -2,22 +2,68 @@ const events = require('events')
 const WebSocket = require('ws')
 const {DateTime} = require('luxon')
 const kindOf = require('kind-of')
-const bybit = require('bybit')
+const {RestClient, WebsocketClient} = require('bybit-api')
 const Bluebird = require('bluebird')
 
-const WEBSOCKET_ENDPOINT = 'wss://stream.bybit.com/realtime'
+class BybitDriver {
+  constructor(opts) {
+    this.exchangeState = {}
+    this.client = new RestClient(opts.key, opts.secret, !!opts.livenet)
+  }
 
-function create(options) {
-  let exchangeState = {}
-  let executedOrders = []
-  const client = bybit({
-    baseURL: options.baseURL,
-    key:     options.key,
-    secret:  options.secret
-  })
+  /**
+   * Establish an authenticated websocket connection and setup handlers
+   * @param {String} market price data requested
+   * @param {Object<String,Function>} handlers
+   */
+  async connect(market, handlers) {
+    const events = ['open', 'reconnected', 'update', 'response', 'close', 'reconnect', 'error']
+    this.ws = new WebsocketClient({ key: this.opts.key, secret: this.opts.secret, livenet: this.opts.livenet })
+    /*
+     * Price needs to be communicated back to the live.Trader (or live.Tester).
+     *   'update'
+     * Order execution needs to be communicated back.
+     *   'response'
+     * Disconnection and reconnection need to be communicated back.
+     *   'close'
+     *   'reconnected'
+     */
 
-  return async function bybit(orders) {
-    let exchangeOrder = {};
+    /*
+     * To make the driver design work for the general case, I need my own events for:
+     * - price
+     * - execution
+     * - reconnected
+     */
+    events.forEach((ev) => {
+      if (handlers[ev]) {
+        this.ws.on(ev, handlers[ev].bind(this))
+      }
+    })
+    this.subscribeCandles(market)
+    if (this.opts.key && this.opts.secret) {
+      this.subscribePrivate()
+    }
+  }
+
+  subscribeCandles(market) {
+    const mkt = market.replace(/\//, '')
+    const channel = `klineV2.1.${mkt}`
+    this.ws.subscribe(channel)
+  }
+
+  subscribePrivate() {
+    this.ws.subscribe(['position', 'execution', 'order', 'stop_order'])
+  }
+
+  /**
+   * Execute a list of order manipulation instructions on the exchange.
+   * Results will come back through websockets.
+   * @param {Array<Object>} orders - Parameter description.
+   */
+  async execute(orders) {
+    const client = this.client
+    let exchangeOrder = {}
     let results = await Bluebird.map(orders, async (order) => {
       switch (order.type) {
       case 'market':
@@ -34,7 +80,7 @@ function create(options) {
         return await client.createOrder(exchangeOrder)
         break;
       case 'modify':
-        const id = exchangeState.localToRemote[order.id] // TODO - Populate localToRemote
+        const id = this.exchangeState.localToRemote[order.id] // TODO - Populate localToRemote
         if (order.action === 'cancel') {
           if (id) {
             return await client.cancelActiveOrder(exchangeOrder)
@@ -52,7 +98,7 @@ function create(options) {
             if (order.quantity) {
               replacementOrder.p_r_qty = order.quantity
             }
-            return await client.post('/open-api/order/replace', replacementOrder)
+            return await client.replaceActiveOrder(replacementOrder)
           } else {
             return new Error(`order.id ${order.id} not found`)
           }
@@ -62,20 +108,89 @@ function create(options) {
         return {} // not implemented yet
       }
     });
-    return [exchangeState, executedOrders]
+    // XXX It doesn't make sense to return exchangeState and executedOrders, because in realtime, they comeback through websockets.
+    return results
   }
-}
 
-const _ping = JSON.stringify({ op: 'ping' })
-function pingAtInterval(ws, interval=30000) {
-  return setInterval(() => {
-    if (ws.readyState) {
-      ws.send(_ping)
+  // Handle websocket events
+
+  /**
+   * Handle the WebSocket client's open event with a custom handler if provided.
+   */
+  handleOpen() {
+    if (this.handlers.open) {
+      this.handlers.open()
     }
-  }, interval)
+  }
+
+  /**
+   * Handle the WebSocket client's open event with a custom handler if provided.
+   */
+  handleClose() {
+    if (this.handlers.close) {
+      this.handlers.close()
+    }
+  }
+
+  /**
+   * Handle the WebSocket client's open event with a custom handler if provided.
+   */
+  handleReconnect() {
+    if (this.handlers.reconnect) {
+      this.handlers.reconnect()
+    }
+  }
+
+  /**
+   * Handle the WebSocket client's open event with a custom handler if provided.
+   */
+  handleReconnected() {
+    if (this.handlers.reconnected) {
+      this.handlers.reconnected()
+    }
+  }
+
+  /**
+   * If an error handler is provided, pass the error to the handler.
+   * @param {Object} error - Parameter description.
+   */
+  handleError(error) {
+    if (this.handlers.error) {
+      this.handlers.error()
+    }
+  }
+
+  // The following two handlers are special in that they aren't simple pass-throughs like the previous handlers.
+  // handleUpdate - consume candles from the websocket and publish them via a 'price' handler if provided.
+  // handleResponse - consume private channels and normalize them into order executions as understood by my strategies.
+
+  /**
+   * Run the candle handler when realtime candles come over the websocket.
+   * @param {Type of update} update - Parameter description.
+   */
+  async handleUpdate(update) {
+    if (this.handlers.onCandle) {
+      const candle = []
+      this.handlers.onCandle(candle)
+    }
+  }
+
+  async handleResponse(response) {
+    if (this.handlers.onExecution) {
+      const execution = {}
+      this.handlers.onExecution(execution)
+    }
+  }
+
 }
 
-function connect(apiKey, endpoint=WEBSOCKET_ENDPOINT) {
+
+function connect(apiKey) {
+  /*
+    // connect, subscribe to klines, and pass kline data through my own eventemitter
+    // return connected client and eventemitter
+    // ---- might want to do something different ----
+
   const ws = new WebSocket(endpoint)
   const ee = new events.EventEmitter()
   ws.on('open', () => {
@@ -111,6 +226,7 @@ function connect(apiKey, endpoint=WEBSOCKET_ENDPOINT) {
   })
 
   return [ws, ee]
+  */
 }
 
 async function subscribeCandles(ws, market) {
@@ -130,9 +246,6 @@ async function subscribeCandles(ws, market) {
   return channel
 }
 
-async function subscribePrivate(ws) {
-}
-
 const limits = {
   maxCandles: 200
 }
@@ -149,15 +262,11 @@ const fees = {
 // However, for trading, I need access to private websocket topics too.
 
 module.exports = {
-  WEBSOCKET_ENDPOINT,
-  create,
-  pingAtInterval,
-  connect,
-  subscribeCandles,
-  subscribePrivate,
   limits,
   fees
 }
+
+module.exports.Driver = BybitDriver
 
 /*
   // instantiate a client
@@ -165,4 +274,41 @@ module.exports = {
   bliv = exchanges.bybit.create({ baseURL: $e.TA_BYBIT_API_URL, key: $e.TA_BYBIT_API_KEY, secret: $e.TA_BYBIT_API_SECRET })
   orders = [{ type: 'limit', action: 'buy', quantity: 1, price: 10500 }]
   bliv(orders).then(cl).catch(console.error)
+
+  $e = process.env
+  bybitAPI = require('bybit-api')
+  client = new bybitAPI.RestClient($e.TA_BYBIT_API_KEY, $e.TA_BYBIT_API_SECRET, false) // false for testnet, true for live
  */
+
+/**
+ * WebSocket Architecture (updated)
+
+   use an eventemitter to communicate from websocket to live.js
+   subscribe to kline to get candles
+     feed candles to livetester or livetrader
+   subscribe to private feeds to get order execution
+     feed order execution results to the same but on a different eventemitter key
+
+   The API I had before (that I actually uesd) was a connect function that returned a [client, eventemitter] pair.
+   I also had subscribeCandles which I used and subscribePrivate which I didn't get around to implementing.
+   I may want to try something different.  I think I need to log into my server and play with this new lib to feel it out.
+   This client emits its own events, and I could maybe do away with my own eventemitter.
+
+   The functions are poorly named, because the idea was not clear in my mind.
+   bybit.create returns an order stream executing function.
+     It should have a more descriptive name than create.
+   bybit.connect returns a websocket client and an eventemitter which might not be needed.
+     I just need a websocket client setup with the right handlers which don't need to change once setup.
+     kline handling should be generic.
+     order handling should be generic too.
+
+   The big problem is that the rest side (hidden behind the order executing function) and the websocket side
+   need to share order data.  I need to be able to map my local ids to their ids.
+
+   How tf do I organize that?
+   There is some client state that may need to get passed around.
+   For example, the mapping between local order ids and exchange order ids may need to be maintained.
+
+   Maybe I should just make a driver class.
+
+   */
