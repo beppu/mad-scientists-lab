9c9166259d2947ac2e36a5f75e972438f1cd6649
John BEPPU <john.beppu@gmail.com>
2021-05-08 16:31:23 -0700

    Remove deprecated functions

--- ta/exchanges/bybit.js
+++ ta/exchanges/bybit.js
@@ -163,31 +163,6 @@ class BybitDriver {
         console.log(exchangeOrder)
         return await client.placeConditionalOrder(exchangeOrder) // FIXME - What method do I really need?
         break;
-      case 'modify':
-        const id = this.exchangeState.localToRemote[order.id] // TODO - Populate localToRemote
-        if (order.action === 'cancel') {
-          if (id) {
-            return await client.cancelActiveOrder(exchangeOrder)
-          } else {
-            return new Error(`order.id ${order.id} not found`)
-          }
-        } else if (order.action === 'update') {
-          if (id) {
-            const replacementOrder = {
-              order_id:  id,
-              symbol:    order.symbol,
-            }
-            if (order.price)
-              replacementOrder.p_r_price = order.price
-            if (order.quantity) {
-              replacementOrder.p_r_qty = order.quantity
-            }
-            return await client.replaceActiveOrder(replacementOrder)
-          } else {
-            return new Error(`order.id ${order.id} not found`)
-          }
-        }
-        break
       default:
         return {} // not implemented yet
       }
@@ -268,67 +243,6 @@ class BybitDriver {
 
 }
 
-function connect(apiKey) {
-  /*
-    // connect, subscribe to klines, and pass kline data through my own eventemitter
-    // return connected client and eventemitter
-    // ---- might want to do something different ----
-
-  const ws = new WebSocket(endpoint)
-  const ee = new events.EventEmitter()
-  ws.on('open', () => {
-    if (apiKey) {
-      // authenticate
-      const expires = DateTime.local() + 1000
-      const signature = "something" // hex(HMAC_SHA256(secret, 'GET/realtime' + expires));
-      const login = JSON.stringify({
-        op: 'auth',
-        args: [ apiKey, expires, signature ]
-      })
-      // ws.send(login)
-    }
-
-    // ask for candles
-    const market = 'BTCUSD'
-    const klineSubscribe = JSON.stringify({
-      op: 'subscribe',
-      args: [ `klineV2.1.${market}` ]
-    })
-    ws.send(klineSubscribe)
-  })
-
-  // setup an emitter that will work for any topic
-  ws.on('message', (payload) => {
-    const data = JSON.parse(payload)
-    const topic = data.topic || 'unknown'
-    ee.emit(topic, data)
-  })
-
-  // TODO - detect disconnections and try to periodically reconnect
-  ws.on('close', () => {
-  })
-
-  return [ws, ee]
-  */
-}
-
-async function subscribeCandles(ws, market) {
-  const mkt = market.replace(/\//, '')
-  const channel = `klineV2.1.${mkt}`
-  const klineSubscribe = JSON.stringify({
-    op: 'subscribe',
-    args: [channel]
-  })
-  if (ws.readyState) {
-    ws.send(klineSubscribe)
-  } else {
-    ws.on('open', () => {
-      ws.send(klineSubscribe)
-    })
-  }
-  return channel
-}
-
 const limits = {
   maxCandles: 200
 }
@@ -337,8 +251,6 @@ const fees = {
   maker: -0.00025, // negative fee is a rebate you get back
   taker: 0.00075
 }
-
-//ws.send(JSON.stringify({ op: 'unsubscribe', args: [ 'klineV2.1.BTCUSD' ] }))
 
 // I'm not sure what the websocket side should look like yet.
 // Initially, I want to pull down public candlestick data that doesn't require authentication.
