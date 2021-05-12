e8c3a511fe34b05ea6e3b1f948bb682febefd4ed
John BEPPU <john.beppu@gmail.com>
2021-05-11 18:52:20 -0700

    Add the ability to set notification title
    GET /a/:sound

--- tvaa2/routes/a/:sound.js
+++ tvaa2/routes/a/:sound.js
@@ -8,7 +8,7 @@ module.exports.GET = async (req, res) => {
   const payload = req.query.message
     ? { sound,
         speech: req.query.message,
-        desktop: { title: 'tvaa2', message: req.query.message } }
+        desktop: { title: req.query.title || 'tvaa2', message: req.query.message } }
     : { sound }
   alert.send(payload)
   send(res, 200, { success: true, sound })
