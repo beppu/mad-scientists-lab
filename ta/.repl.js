d6a54925730c5844a31f90d6f652a6e631a41163
John BEPPU <john.beppu@gmail.com>
2021-06-13 13:48:38 -0700

    Add Lazy and sprintf to the repl

--- ta/.repl.js
+++ ta/.repl.js
@@ -10,10 +10,10 @@ global.rl       = reload
 global.cl       = console.log
 
 // global.Promise     = require('bluebird')
-// global.Lazy        = require('lazy.js')
-// global.moment      = require('moment')
-// global.sprintf     = require('sprintf')
-// global.outdent     = require('outdent')
+global.Lazy        = require('lazy.js')
+//global.moment      = require('moment')
+global.sprintf     = require('sprintf')
+//global.outdent     = require('outdent')
 global.ccxt        = require('ccxt')
 global.talib       = require('talib')
 global.luxon       = require('luxon')
