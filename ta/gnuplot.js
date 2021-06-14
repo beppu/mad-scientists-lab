b958cb3586486ef2edf799fe4145a4aef8f5d2a4
John BEPPU <john.beppu@gmail.com>
2021-06-14 06:56:24 -0700

    Add gnuplot.findOldestIndex(imd)
    - I also changed gnuplot.writeCandles to gnuplot.writeImd, because it'll be
      writing out more than candles.
    - gnuplot.writeImd also gained the ability to provide blanks for not-yet-defined
      data by using "-".
    
      In gnuplot, I have to say:
    
        set datafile missing "-"

--- ta/gnuplot.js
+++ ta/gnuplot.js
@@ -15,13 +15,35 @@ const pipeline = require('./pipeline')
 const time = require('./time')
 const {DateTime} = require('luxon')
 
+/**
+ * Find the index where all imd series have a defined value
+ * @param {InvertedMarketData} imd - inverted market data
+ * @returns {Number} index in imd
+ */
 function findFullKeysIndex(imd) {
   const lengths = Object.keys(imd).map((key) => imd[key].length)
   const shortest = Math.min(...lengths)
   return shortest - 1
 }
 
-function writeCandles(imd, outputStream, begin, end=0) {
+/**
+ * The earliest index available in imd.
+ * @param {InvertedMarketData} imd - inverted market data
+ * @returns {Number} index in imd
+ */
+function findOldestIndex(imd) {
+  // imd.timestamp should always be the longest series.
+  return imd.timestamp.length - 1
+}
+
+/**
+ * Write imd out in gnuplot format
+ * @param {InvertedMarketData} imd - inverted market data
+ * @param {Stream} outputStream - io stream to write to.
+ * @param {Number} begin - (optional) index to start writing
+ * @param {Number} end - (optional) index to stop writing (default: 0)
+ */
+function writeImd(imd, outputStream, begin, end=0) {
   const keys = Object.keys(imd)
   const realBegin = begin ? begin : findFullKeysIndex(imd)
   // header
@@ -35,7 +57,7 @@ function writeCandles(imd, outputStream, begin, end=0) {
         const ts = time.isoUTC(imd.timestamp[i]).replace(/\.000Z/, '')
         outputStream.write(`${ts} `)
       } else {
-        outputStream.write(`${imd[k][i]} `)
+        outputStream.write(`${imd[k][i] || '-'} `)
       }
     })
     outputStream.write("\n")
@@ -49,7 +71,8 @@ function writeCandles(imd, outputStream, begin, end=0) {
 
 module.exports = {
   findFullKeysIndex,
-  writeCandles
+  findOldestIndex,
+  writeImd
 }
 
 
