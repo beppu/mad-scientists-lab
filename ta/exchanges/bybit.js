427b9b2b30d51216fa1ec91ff043e4007397247c
John BEPPU <john.beppu@gmail.com>
2021-06-29 12:14:32 -0700

    Remove obsolete comments

--- ta/exchanges/bybit.js
+++ ta/exchanges/bybit.js
@@ -301,65 +301,15 @@ const fees = {
   taker: 0.00075
 }
 
-// I'm not sure what the websocket side should look like yet.
-// Initially, I want to pull down public candlestick data that doesn't require authentication.
-// However, for trading, I need access to private websocket topics too.
-
 module.exports = {
   limits,
   fees,
   Driver: BybitDriver
 }
 
-/*
-  // OLD
-  // instantiate a client
-  $e = process.env
-  bliv = exchanges.bybit.create({ baseURL: $e.TA_BYBIT_API_URL, key: $e.TA_BYBIT_API_KEY, secret: $e.TA_BYBIT_API_SECRET })
-  orders = [{ type: 'limit', action: 'buy', quantity: 1, price: 10500 }]
-  bliv(orders).then(cl).catch(console.error)
-
-  $e = process.env
-  BybitAPI = require('bybit-api')
-  client = new BybitAPI.RestClient($e.TA_BYBIT_API_KEY, $e.TA_BYBIT_API_SECRET, false) // false for testnet, true for live
- */
-
 /**
- * WebSocket Architecture (updated)
 
-   use an eventemitter to communicate from websocket to live.js
-   subscribe to kline to get candles
-     feed candles to livetester or livetrader
-   subscribe to private feeds to get order execution
-     feed order execution results to the same but on a different eventemitter key
-
-   The API I had before (that I actually uesd) was a connect function that returned a [client, eventemitter] pair.
-   I also had subscribeCandles which I used and subscribePrivate which I didn't get around to implementing.
-   I may want to try something different.  I think I need to log into my server and play with this new lib to feel it out.
-   This client emits its own events, and I could maybe do away with my own eventemitter.
-
-   The functions are poorly named, because the idea was not clear in my mind.
-   bybit.create returns an order stream executing function.
-     It should have a more descriptive name than create.
-   bybit.connect returns a websocket client and an eventemitter which might not be needed.
-     I just need a websocket client setup with the right handlers which don't need to change once setup.
-     kline handling should be generic.
-     order handling should be generic too.
-
-   The big problem is that the rest side (hidden behind the order executing function) and the websocket side
-   need to share order data.  I need to be able to map my local ids to their ids.
-
-   How tf do I organize that?
-   There is some client state that may need to get passed around.
-   For example, the mapping between local order ids and exchange order ids may need to be maintained.
-
-   Maybe I should just make a driver class.
-
-*/
-
-/**
- * New
-
+ // Instantiate the driver
  key = process.env.TA_BYBIT_API_KEY
  secret = process.env.TA_BYBIT_API_SECRET
  livenet = false
