b13882b2ef7ab26ed7707eaab98346af2a9184cb
John BEPPU <john.beppu@gmail.com>
2021-07-07 19:39:34 -0700

    Make note of my WMA research findings

--- ta/indicators/hma.js
+++ ta/indicators/hma.js
@@ -10,7 +10,9 @@ function keyName(period) {
 module.exports = function hmaFn(period=55) {
   const key = keyName(period)
 
-  const hmaIterate = function(imd, state) {
+  const sqPeriod = Math.round(Math.sqrt(period))
+
+  const hmaIterate = function(md, imd, state) {
     // TODO Implement
     // NOTE rsi.js used md to make it easier to port the C code from talib to JS.  hma.js should be able to use imd instead.
     const newState = {}
@@ -42,5 +44,28 @@ module.exports = function hmaFn(period=55) {
    wma2 = wma(values, window)
    hull_moving_averages = wma((wma1 - wma2), period)
    return hull_moving_averages
+
+
+   // WMA research
+   // Does it need data beyond its declared period (like EMA)?
+   s = []
+   for (i=0; i<10; i++) { s.push(100) }
+   for (i=1; i<11; i++) { s.push(100*i) }
+   ss = s.map((close, i) => [i, 0, 0, 0, close, 0])
+   md = ta.marketDataFromCandles(ss)
+   settings = ta.id.wma(md, 10)
+   r = talib.execute(settings)
+
+   settings2 = clone(settings)
+   settings2.inReal.splice(0, 5) // remove first 5 items
+   settings2.endIdx = 14 // adjust params to end at right index
+   r2 = talib.execute(settings2)
+
+   // r.result and r2.result match up if you look at them from the end.
+
+   // DUDE!  It looks like WMAs do *NOT* need data beyond
+   // their declared period.  This means I can use talib for WMA and HMA,
+   // and it won't be too computationally disgusting.  I can make
+   // it faster later if necessary.
 
    */
