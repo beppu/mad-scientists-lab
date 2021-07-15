7b58836323722e8ca01cb75dab70e18441592d07
John BEPPU <john.beppu@gmail.com>
2021-07-15 09:05:04 -0700

    Acknowledge that some functions mutate their parameters
    ...and don't pretend otherwise.
    Also changed some "[]" notation to "." notation.

--- ta/indicators/sma.unit.test.js
+++ ta/indicators/sma.unit.test.js
@@ -58,14 +58,14 @@ test('SMA stream calculations should be consistent with SMA batch calculations',
   const [smaInsert, smaUpdate] = sma(20)
   let state
   candles.forEach((c) => {
-    md = ta.marketDataAppendCandle(md, c)
-    imd = ta.invertedAppendCandle(imd, c)
+    ta.marketDataAppendCandle(md, c)
+    ta.invertedAppendCandle(imd, c)
     state = smaInsert(md, imd, state)
   })
 
   // batch calculation copied and adapted from bin/price
   const marketData         = ta.marketDataFromCandles(candles)
-  const indicatorSettings  = ta.id['sma'](marketData, 20)
+  const indicatorSettings  = ta.id.sma(marketData, 20)
   const r                  = talib.execute(indicatorSettings)
   const invertedMarketData = ta.invertedMarketData(marketData)
   ta.invertedAppend(invertedMarketData, 'sma20', r.result.outReal)
