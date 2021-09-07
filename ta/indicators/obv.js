2e696ca5e6eff742018f0bca3039ac012285476b
Keith Hörling <keith@Slaptop-Sr.local>
2021-09-07 14:47:19 -0700

    fix: update replaces & use anchor to name

--- ta/indicators/obv.js
+++ ta/indicators/obv.js
@@ -5,10 +5,10 @@ const {ha} = require('../analysis/candles')
 // TODO indicate divergent trends beween volume & price?
 // - should that be part of strategy or internal for ease-of-use?
 
-const keyName = period => `obv${period}`
+const keyName = anchor => `obv${anchor}`
 
-module.exports = function obvFn(period=55) {
-  const key = keyName(period)
+module.exports = function obvFn(anchor='') {
+  const key = keyName(anchor)
 
   const obvIterate = function(md, imd, state) {
     const
@@ -17,7 +17,7 @@ module.exports = function obvFn(period=55) {
       cur     = imd.close[0]  || 0,
       last    = imd.close[1]  || 0
     // TODO how can we reset based on anchors?
-    // if ((period | peak | valley | price) % N === 0) lastObv = 0
+    // if ((anchor | peak | valley | price) % N === 0) lastObv = 0
     if (cur === last) {
       // obv remains unchanged
       return lastObv
@@ -31,23 +31,22 @@ module.exports = function obvFn(period=55) {
 
   const obvInsert = function(md, imd, state) {
     // create new candle
-    if (md.close.length < period) return undefined // not yet enough data
+    if (md.close.length < 1) return undefined // not yet enough data
     // insert initial obv value
     const obv = obvIterate(md, imd, state)
     if (imd[key]) {
       imd[key].unshift(obv)
     } else {
-      imd[key] = []
-      imd[key].unshift(obv)
+      imd[key] = [obv]
     }
     return { timestamp: imd.timestamp[0] }
   }
 
   const obvUpdate = function(md, imd, state) {
     // update existing candle
-    if (md.close.length < period+1) return undefined // " guard
+    if (md.close.length < 1) return undefined // " guard
     // update obv value
-    imd[key].unshift(obvIterate(md, imd, state))
+    imd[key] = obvIterate(md, imd, state)
     return { timestamp: imd.timestamp[0] }
   }
 
