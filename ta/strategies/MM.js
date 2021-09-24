f46f0e461550caf3e4a3f5f34e40da01f949c716
John BEPPU <john.beppu@gmail.com>
2021-09-24 11:56:06 -0700

    Escape backslashes in gnuplot script

--- ta/strategies/MM.js
+++ ta/strategies/MM.js
@@ -164,10 +164,10 @@ set timefmt "%Y-%m-%dT%H:%M:%S"
 set y2tics
 set boxwidth 0.7 relative
 plot [][{{low}}:{{high}}] "{{config.trendTf}}.data" skip {{skip}} using 1:7:8:9:10 title "BTC/USD Heikin Ashi" with candlesticks, \\
-  "" skip 0 using 1:14 title "HMA 330" with line lw 3 lc rgb "#26c6da",  \
-  "" skip 0 using 1:15 title "HMA 440" with line lw 3 lc rgb "#64b5f6",  \
-  "" skip 0 using 1:11 title "4h upper band" with line lw 3 lc rgb "#4c51da", \
-  "1d.data" skip 0 using 1:11 title "1d upper band" with line lw 3 lc rgb "purple", \
+  "" skip 0 using 1:14 title "HMA 330" with line lw 3 lc rgb "#26c6da",  \\
+  "" skip 0 using 1:15 title "HMA 440" with line lw 3 lc rgb "#64b5f6",  \\
+  "" skip 0 using 1:11 title "4h upper band" with line lw 3 lc rgb "#4c51da", \\
+  "1d.data" skip 0 using 1:11 title "1d upper band" with line lw 3 lc rgb "purple", \\
   "orders.data" using 1:2:(stringcolumn(4) eq "buy" ? 9 : 11) title "Orders" with points pointsize 3 pt var lc rgb "orange", \\
   "pnl.data" using 1:2 axes x1y2 title "Equity" with line lw 3 lc rgb "green"
 `
